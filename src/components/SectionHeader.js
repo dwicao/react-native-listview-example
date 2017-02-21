73df9e72a57071a633ccfcc2b2ebf44b3bd464ac
dwicao <lutfiandwica@gmail.com>
2017-02-21 09:21:51 +0700

    add SectionHeader component

---
+++ src/components/SectionHeader.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {
+	View,
+	Text,
+	StyleSheet,
+} from 'react-native';
+
+const SectionHeader = props => {
+	<View style={styles.container}>
+		<Text style={styles.text}>{props.character}</Text>
+	</View>
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		padding: 8,
+		justifyContent: 'center',
+		backgroundColor: '#EAEAEA',
+	},
+	text: {
+		fontSize: 13,
+	},
+});
+
+export default SectionHeader;
